6eb726bc2fd954ce30b80c92b1345d575dae99c6
diar <diar.selimi@nordsec.com>
2023-09-05 22:34:40 +0200

    some style improvements

---
+++ src/components/Author.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="footer container p-1 center-text">
+      <p>
+        Created by 
+        <a href="https://erdemkose.com/" class="link link-primary" target="_blank">Erdem Köse</a>
+      </p>
+    </div>
+  </template>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import Author from '../components/Author.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
   </main>
+  <Author />
 </template>
